ec0aa445cf97e5912e4e448cbbfba977d064e216
rz777 <61600538+rz777@users.noreply.github.com>
2022-01-20 12:03:48 +0100

    Update burnaxies.js

--- burnaxies.js
+++ burnaxies.js
@@ -11,18 +11,20 @@
     // sleep timer, recommended minimum 11000 (3 blocks), better use 31000 (10 blocks+)
     var sleepTime = 31000;
     // private key of source (0x...)
-    var privkey = '0x1337deafbeef';
+    var privkey = '0x1337';
 // ******* END CONFIG *******
 const ethers = require('ethers');
 const utils = require('ethers-utils');
 const fetch = require('node-fetch');
 const web3 = require('web3');
 const __CONTRACT_BURN = '0x144697847f4bf184534af5945abe0fb5f1b14fba';
+const __ABI_BURN = require('./ABIBURN.json');
 const __URL_RPC_RONIN = 'https://proxy.roninchain.com/free-gas-rpc';
 const __ID_CHAIN_RONIN = 2020;
 const __GAS_PRICE = 0;
 const __GAS_LIMIT = 1000000;
 var wallet;
+var contract;
 // ZzZzZzZ
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -41,12 +43,40 @@ getWalletfromPrivateKey = (provider, private_key) => {
 };
 // WALLET OBJECT
 wallet = getWalletfromPrivateKey(__PROVIDER_RONIN, privkey);
+contract = new ethers.Contract(__CONTRACT_BURN, __ABI_BURN, wallet);
 // MAIN FUNCTION
 async function burnAxies(from, size) {
     var graph = `https://axieinfinity.com/graphql-server-v2/graphql?query={axies(from:${from},size:${size},sort:IdAsc,criteria:{stages:[4]},owner:"${wallet.address}"){results{id,breedCount,auction{listingIndex}}}}`;
     var json = (await fetch(graph).then(response=>response.json()));
     var axies = json.data.axies.results;
-    var str = '0000000000000000000000000000000000000000000000000000000000000000';    
+
+    // ------- BATCH AXIE BURNING -------
+    // BURN max 5 Axies per 1000000 gas
+    var maxAxies = 5;
+    var allAxies = [];
+    console.log(`BURNING TOTAL ${axies.length} AXIES...`);
+    for (var i=0; i<axies.length; i++) {
+        allAxies.push(axies[i].id);
+        if (allAxies.length == maxAxies) {
+            console.log(allAxies);
+            try {
+                var res = (await contract.releaseAxies(
+                    allAxies,
+                    { gasPrice: __GAS_PRICE, gasLimit: __GAS_LIMIT }
+                ));
+                console.log(`-> ${res.hash}`);
+            } catch (e) {
+                console.log(`-> ERROR: ${e.reason}`);
+            };
+            allAxies = [];
+            (await sleep(sleepTime));
+        }
+    }
+
+    // ------- SINGLE AXIE BURNING -------
+    /*
+    var str = '0000000000000000000000000000000000000000000000000000000000000000';
+    console.log(`BURNING TOTAL ${axies.length} AXIES...`);
     for (var i=0; i<axies.length; i++) {
         console.log(`BURNING AXIE: ${axies[i].id}`);
         hexAxieID = web3.utils.toHex(axies[i].id).replace("0x", "");
@@ -66,7 +96,9 @@ async function burnAxies(from, size) {
             console.log(`-> ERROR! ${e.reason}`);
         }
         (await sleep(sleepTime));
-    };
+    }
+    */
+    
     return;
 };
 // RUN MIGRATION
